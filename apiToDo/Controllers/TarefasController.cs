b6771abd36dbd1c61116cca148b2dfb50adfee98
Bruno-Henrique-P <brunoh5406@gmail.com>
2025-04-26 18:04:27 -0300

    oraganização do controller

--- apiToDo/Controllers/TarefasController.cs
+++ apiToDo/Controllers/TarefasController.cs
@@ -3,6 +3,8 @@ using apiToDo.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace apiToDo.Controllers
 {
@@ -10,18 +12,24 @@ namespace apiToDo.Controllers
     [Route("[controller]")]
     public class TarefasController : ControllerBase
     {
-        [Authorize]
+        
+        // crio um obj Tarefa para utilizar em todos corpo da requisição
+       
+        //[Authorize]
+
         [HttpPost("lstTarefas")]
         public ActionResult lstTarefas()
         {
             try
             {
-              
-                return StatusCode(200);
+                Tarefas tarefas = new Tarefas();
+
+                return Ok(tarefas.getTarefaAll());
             }
 
             catch (Exception ex)
             {
+
                 return StatusCode(400, new { msg = $"Ocorreu um erro em sua API {ex.Message}"});
             }
         }
@@ -31,9 +39,19 @@ namespace apiToDo.Controllers
         {
             try
             {
-
-                return StatusCode(200);
-
+                Tarefas tarefas = new Tarefas();
+                
+                /// tratamento para id de tarefa já existente
+                if (tarefas.getTarefa(Request.ID_TAREFA) == null)
+                {
+                    tarefas.InserirTarefa(Request);
+                    return Ok(tarefas.getTarefaAll());
+                }
+                /// explico erro no terminal 
+                else
+                {
+                    throw new Exception($"Tarefa com Id = {Request.ID_TAREFA} já existe");
+                }
 
             }
 
@@ -48,8 +66,9 @@ namespace apiToDo.Controllers
         {
             try
             {
-
-                return StatusCode(200);
+                Tarefas tarefas = new Tarefas();
+                tarefas.DeletarTarefa(ID_TAREFA);
+                return Ok(200);
             }
 
             catch (Exception ex)
@@ -57,5 +76,26 @@ namespace apiToDo.Controllers
                 return StatusCode(400, new { msg = $"Ocorreu um erro em sua API {ex.Message}" });
             }
         }
+        [HttpGet("PegarTarefa")]
+        public ActionResult PegarTarefa([FromQuery] int ID_TAREFA)
+        {
+            try
+            {
+                Tarefas tarefas = new Tarefas();
+                if (tarefas.getTarefa(ID_TAREFA) == null)
+                {
+                    return StatusCode(400, new {msg = $"Tarefa com ID {ID_TAREFA} não encontrada." });
+                }
+               
+                return Ok(tarefas.getTarefa(ID_TAREFA));
+                    
+                }
+
+            catch (Exception ex)
+            {
+                return StatusCode(400, new { msg = $"Ocorreu um erro em sua API {ex.Message}" });
+            }
+        }
+
     }
 }
--- apiToDo/Models/Tarefas.cs
+++ apiToDo/Models/Tarefas.cs
@@ -7,44 +7,83 @@ namespace apiToDo.Models
 {
     public class Tarefas
     {
+        // criei a variavel private static tarefas onde as tarefas criadas no lstTarefas vao ser armazenadas e podem ser manipuladas por qualquer função da classe
+        private static List<TarefaDTO> tarefas = new List<TarefaDTO>();
+        // metodo para criar as terafas padrao
         public List<TarefaDTO> lstTarefas()
         {
             try
             {
-                List<TarefaDTO> lstTarefas = new List<TarefaDTO>();
-
-                lstTarefas.Add(new TarefaDTO
+                
+                tarefas.Add(new TarefaDTO
                 {
                     ID_TAREFA = 1,
                     DS_TAREFA = "Fazer Compras"
                 });
 
-                lstTarefas.Add(new TarefaDTO
+                tarefas.Add(new TarefaDTO
                 {
                     ID_TAREFA = 2,
                     DS_TAREFA = "Fazer Atividad Faculdade"
                 });
 
-                lstTarefas.Add(new TarefaDTO
+                tarefas.Add(new TarefaDTO
                 {
                     ID_TAREFA = 3,
                     DS_TAREFA = "Subir Projeto de Teste no GitHub"
                 });
-
-                return new List<TarefaDTO>();
+                // alterei a variavel pois estava retornando uma lista vazia agora retorna a lista atualizada
+                return tarefas;
             }
             catch(Exception ex)
             {
                 throw ex;
             }
         }
+        /// meotodo criada para retorna a lista interia, subtitui em todos os metodos, já que toda vez que o lstTarefa rodava a lista resetava, agora é possivel manipular
+        public List<TarefaDTO> getTarefaAll()
+        {
+            try
+            {
+                /// aqui faço um verificação para saber se os valores padrao já foram adicionados, se a lista estiver vazia ele cria os valores se conter algum item ele não cria nada 
+                if (tarefas.Count == 0)
+                {
+                    lstTarefas();
+                }
+                /// retorna a lista de tarefas(variavel da classe)
+                return tarefas;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        /// metodo para retornar um tarefa especifica da lista, similar ao metodo DeletarTarefa porém na ultima linha ao inves de deltar o elemento ele retorna o mesmo
+        public TarefaDTO getTarefa(int ID_TAREFA)
+        {
+            try
+            {
+                List<TarefaDTO> lstResponse = getTarefaAll();
+                var Tarefa = lstResponse.FirstOrDefault(x => x.ID_TAREFA == ID_TAREFA);
+                if (Tarefa == null)
+                {
+                    throw new Exception($"Tarefa com Id = {ID_TAREFA} já existe");
+                }
+                TarefaDTO Tarefa2 = lstResponse.Where(x => x.ID_TAREFA == Tarefa.ID_TAREFA).FirstOrDefault();
+                return Tarefa2;
+            }
+            catch(Exception ex)
+            { 
+                throw ex; 
+            }
+        } 
 
 
         public void InserirTarefa(TarefaDTO Request)
         {
             try
             {
-                List<TarefaDTO> lstResponse = lstTarefas();
+                List<TarefaDTO> lstResponse = getTarefaAll();
                 lstResponse.Add(Request);
             }
             catch(Exception ex)
@@ -56,8 +95,12 @@ namespace apiToDo.Models
         {
             try
             {
-                List<TarefaDTO> lstResponse = lstTarefas();
+                List<TarefaDTO> lstResponse = getTarefaAll();
                 var Tarefa = lstResponse.FirstOrDefault(x => x.ID_TAREFA == ID_TAREFA);
+                if (Tarefa == null)
+                {
+                    throw new Exception($"Tarefa com Id = {ID_TAREFA} não existe");
+                }
                 TarefaDTO Tarefa2 = lstResponse.Where(x=> x.ID_TAREFA == Tarefa.ID_TAREFA).FirstOrDefault();
                 lstResponse.Remove(Tarefa2);
             }
